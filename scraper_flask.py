8ceaca52dc77a1f53d2b88026cc958e56ef05dea
atay.kaylar1 <atay.kaylar@onbiron.com.tr>
2025-06-02 18:13:58 +0300

    Update headers to bypass Akakce 403

--- scraper_flask.py
+++ scraper_flask.py
@@ -5,7 +5,10 @@ from bs4 import BeautifulSoup
 app = Flask(__name__)
 
 def scrape_akakce_for_price(query, max_results=20):
-    headers = {"User-Agent": "Mozilla/5.0"}
+    headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+    "Accept-Language": "en-US,en;q=0.9",
+}
     search_url = f"https://www.akakce.com/arama/?q={query.replace(' ', '+')}"
 
     try:
